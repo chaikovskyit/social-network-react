83e51cd23e98f6f3065db0bee2025b1633db2366
chaikovskyit <chaikovskyit@gmail.com>
2020-12-10 18:00:27 +0200

    add browserrout and navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import s from './Navbar.module.css'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return(
     <nav className={s.nav}>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">Profile</a>
+        <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">Messages</a>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">News</a>
+        <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">Music</a>
+        <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">Settings</a>
+        <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
       </div>
     </nav>
   )
