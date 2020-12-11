b2082301ce4bde88ec6ecae4cbc0014240b3ec37
chaikovskyit <chaikovskyit@gmail.com>
2020-12-11 16:50:23 +0200

    use route for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import DialogItem from './DialogItem/DialogItem'
+import Message from './Message/Message'
+
+
 
 const Dialogs = () => {
   return(
     <div className={s.dialogs}>
-      Dialogs
+      <div className={s.dialogsItems}>
+        <DialogItem name="Yoda" id="1"/>
+        <DialogItem name="DarthWaider" id="2"/>
+        <DialogItem name="R2D2" id="3"/>
+      </div>
+      <div className={s.messages}>
+        <Message message="Hello"/>
+        <Message message="Hi"/>
+        <Message message="Yo"/>
+      </div>
     </div>
   )
 }
